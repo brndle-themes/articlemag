760152f426e59a31c319abe47e0f231805ceeb6c
Wbcom Dev <pallavi@wbcomdesigns.com>
2020-03-04 19:14:55 +0530

    Fixed nav Menu Location Issue and Fixed License Key Issues

--- inc/cs-framework/assets/js/csf.js
+++ inc/cs-framework/assets/js/csf.js
@@ -2356,6 +2356,13 @@
                 }, 1000);
               });
 
+                /*License Key activate or deactivate then refresh page */
+                if ( typeof(response.page_reload) !== 'undefined'  ) {         
+                  setTimeout(function(){
+                     location.reload(); 
+                  }, 500);
+                }
+
             })
             .fail( function( response ) {
               alert( response.error );
