178bcf23c5501d40265201784f7db240096b8ee8
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2020-07-21 13:01:44 +0530

    Register sidebar for buddypress pages

--- inc/classes/class-cs-sidebar-api.php
+++ inc/classes/class-cs-sidebar-api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * CSFramework Sidebars API
@@ -8,213 +9,259 @@
  */
 class CSFramework_Sidebars_API {
 
-	public function __construct() {
-		$this->register_default_sidebars();
-		$this->register_footer_sidebars();
-		$this->register_custom_sidebars();
-	}
+    public function __construct() {
+        $this->register_default_sidebars();
+        if (class_exists('buddypress') && (!is_plugin_active('youzer/youzer.php'))) {
+            $this->register_buddypress_sidebars();
+        }
+        $this->register_footer_sidebars();
+        $this->register_custom_sidebars();
+    }
 
-	/**
-	 *
-	 * Register Default Sidebars
-	 *
-	 * @since 1.0.0
-	 * @version 1.0.0
-	 */
-	public function register_default_sidebars() {
+    /**
+     *
+     * Register Default Sidebars
+     *
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    public function register_default_sidebars() {
 
-		$defaults = array(
-			'sidebar-1' => 'Primary Sidebar',
-			'sidebar-2' => 'Secondary Sidebar',
-		);
+        $defaults = array(
+            'sidebar-1' => 'Primary Sidebar',
+            'sidebar-2' => 'Secondary Sidebar',
+        );
 
-		foreach ( $defaults as $key => $name ) {
-			register_sidebar(
-				array(
-					'id'            => $key,
-					'name'          => $name,
-					'description'   => 'Drag widgets for all of pages sidebar',
-					'before_widget' => '<div class="articlemag_widget %2$s">',
-					'after_widget'  => '<div class="clear"></div></div>',
-					'before_title'  => '<div class="widget-title"><h4>',
-					'after_title'   => '</h4></div>',
-				)
-			);
-		}
+        foreach ($defaults as $key => $name) {
+            register_sidebar(
+                    array(
+                        'id' => $key,
+                        'name' => $name,
+                        'description' => 'Drag widgets for all of pages sidebar',
+                        'before_widget' => '<div class="articlemag_widget %2$s">',
+                        'after_widget' => '<div class="clear"></div></div>',
+                        'before_title' => '<div class="widget-title"><h4>',
+                        'after_title' => '</h4></div>',
+                    )
+            );
+        }
+    }
 
-	}
+    /**
+     *
+     * Register BuddyPress Sidebars
+     *
+     * @since 1.5.0
+     * @version 1.5.0
+     */
+    public function register_buddypress_sidebars() {
 
-	/**
-	 *
-	 * Register Custom Sidebars
-	 *
-	 * @since 1.0.0
-	 * @version 1.0.0
-	 */
-	public function register_custom_sidebars() {
+        register_sidebar(
+                array(
+                    'id' => 'activity_directory',
+                    'name' => 'Activity &rarr; Directory',
+                    'description' => 'Drag widgets for activity directory sidebar',
+                    'before_widget' => '<div class="articlemag_widget %2$s">',
+                    'after_widget' => '<div class="clear"></div></div>',
+                    'before_title' => '<div class="widget-title"><h4>',
+                    'after_title' => '</h4></div>',
+                )
+        );
 
-		$get_custom_sidebars = cs_get_option( 'sidebars' );
+        register_sidebar(
+                array(
+                    'id' => 'members_directory',
+                    'name' => 'Members &rarr; Directory',
+                    'description' => 'Drag widgets for members directory sidebar',
+                    'before_widget' => '<div class="articlemag_widget %2$s">',
+                    'after_widget' => '<div class="clear"></div></div>',
+                    'before_title' => '<div class="widget-title"><h4>',
+                    'after_title' => '</h4></div>',
+                )
+        );
 
-		if ( $get_custom_sidebars ) {
+        register_sidebar(
+                array(
+                    'id' => 'groups_directory',
+                    'name' => 'Groups &rarr; Directory',
+                    'description' => 'Drag widgets for groups directory sidebar',
+                    'before_widget' => '<div class="articlemag_widget %2$s">',
+                    'after_widget' => '<div class="clear"></div></div>',
+                    'before_title' => '<div class="widget-title"><h4>',
+                    'after_title' => '</h4></div>',
+                )
+        );
+    }
 
-			foreach ( $get_custom_sidebars as $key => $sidebar ) {
+    /**
+     *
+     * Register Custom Sidebars
+     *
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    public function register_custom_sidebars() {
 
-				$sidebar_name = $sidebar['sidebar_name'];
-				register_sidebar(
-					array(
-						'id'            => sanitize_title( $sidebar_name ),
-						'name'          => $sidebar_name,
-						'description'   => 'Drag widgets for all of pages sidebar',
-						'before_widget' => '<div class="articlemag_widget %2$s">',
-						'after_widget'  => '<div class="clear"></div></div>',
-						'before_title'  => '<div class="widget-title"><h4>',
-						'after_title'   => '</h4></div>',
-					)
-				);
+        $get_custom_sidebars = cs_get_option('sidebars');
 
-			}
-		}
-	}
+        if ($get_custom_sidebars) {
 
+            foreach ($get_custom_sidebars as $key => $sidebar) {
 
-	/**
-	 *
-	 * Register Footer Sidebars
-	 *
-	 * @since 1.0.0
-	 * @version 1.0.1
-	 */
-	public function register_footer_sidebars() {
+                $sidebar_name = $sidebar['sidebar_name'];
+                register_sidebar(
+                        array(
+                            'id' => sanitize_title($sidebar_name),
+                            'name' => $sidebar_name,
+                            'description' => 'Drag widgets for all of pages sidebar',
+                            'before_widget' => '<div class="articlemag_widget %2$s">',
+                            'after_widget' => '<div class="clear"></div></div>',
+                            'before_title' => '<div class="widget-title"><h4>',
+                            'after_title' => '</h4></div>',
+                        )
+                );
+            }
+        }
+    }
 
-		if ( cs_get_option( 'footer_block_before' ) ) {
-			register_sidebar(
-				array(
-					'id'            => 'footer-block-before',
-					'name'          => 'Footer Block Before',
-					'description'   => 'Drag widgets for all of pages sidebar',
-					'before_widget' => '<div class="articlemag_widget %2$s">',
-					'after_widget'  => '<div class="clear"></div></div>',
-					'before_title'  => '<div class="widget-title"><h4>',
-					'after_title'   => '</h4></div>',
-				)
-			);
-		}
+    /**
+     *
+     * Register Footer Sidebars
+     *
+     * @since 1.0.0
+     * @version 1.0.1
+     */
+    public function register_footer_sidebars() {
 
-		if ( cs_get_option( 'footer_before' ) ) {
-			register_sidebar(
-				array(
-					'id'            => 'footer-before',
-					'name'          => 'Footer Widgets Before',
-					'description'   => 'Drag widgets for all of pages sidebar',
-					'before_widget' => '<div class="articlemag_widget %2$s">',
-					'after_widget'  => '<div class="clear"></div></div>',
-					'before_title'  => '<div class="widget-title"><h4>',
-					'after_title'   => '</h4></div>',
-				)
-			);
-		}
+        if (cs_get_option('footer_block_before')) {
+            register_sidebar(
+                    array(
+                        'id' => 'footer-block-before',
+                        'name' => 'Footer Block Before',
+                        'description' => 'Drag widgets for all of pages sidebar',
+                        'before_widget' => '<div class="articlemag_widget %2$s">',
+                        'after_widget' => '<div class="clear"></div></div>',
+                        'before_title' => '<div class="widget-title"><h4>',
+                        'after_title' => '</h4></div>',
+                    )
+            );
+        }
 
-		$sidebars = cs_get_option( 'footer_widgets' );
+        if (cs_get_option('footer_before')) {
+            register_sidebar(
+                    array(
+                        'id' => 'footer-before',
+                        'name' => 'Footer Widgets Before',
+                        'description' => 'Drag widgets for all of pages sidebar',
+                        'before_widget' => '<div class="articlemag_widget %2$s">',
+                        'after_widget' => '<div class="clear"></div></div>',
+                        'before_title' => '<div class="widget-title"><h4>',
+                        'after_title' => '</h4></div>',
+                    )
+            );
+        }
 
-		if ( $sidebars ) {
+        $sidebars = cs_get_option('footer_widgets');
 
-			switch ( $sidebars ) {
-				case 5:
-					$length = 6;
-					break;
+        if ($sidebars) {
 
-				case 6:
-				case 7:
-				case 8:
-					$length = 3;
-					break;
+            switch ($sidebars) {
+                case 5:
+                    $length = 6;
+                    break;
 
-				case 9:
-				case 10:
-					$length = 4;
-					break;
+                case 6:
+                case 7:
+                case 8:
+                    $length = 3;
+                    break;
 
-				default:
-					$length = $sidebars;
-					break;
-			}
+                case 9:
+                case 10:
+                    $length = 4;
+                    break;
 
-			for ( $i = 0; $i < $length; $i++ ) {
+                default:
+                    $length = $sidebars;
+                    break;
+            }
 
-				$num = ( $i + 1 );
+            for ($i = 0; $i < $length; $i++) {
 
-				register_sidebar(
-					array(
-						'id'            => 'footer-' . $num,
-						'name'          => 'Footer Widget ' . $num,
-						'description'   => 'Appears in the footer section of the site',
-						'before_widget' => '<div class="articlemag_widget %2$s">',
-						'after_widget'  => '<div class="clear"></div></div>',
-						'before_title'  => '<div class="widget-title"><h4>',
-						'after_title'   => '</h4></div>',
-					)
-				);
+                $num = ( $i + 1 );
 
-			}
-		}
+                register_sidebar(
+                        array(
+                            'id' => 'footer-' . $num,
+                            'name' => 'Footer Widget ' . $num,
+                            'description' => 'Appears in the footer section of the site',
+                            'before_widget' => '<div class="articlemag_widget %2$s">',
+                            'after_widget' => '<div class="clear"></div></div>',
+                            'before_title' => '<div class="widget-title"><h4>',
+                            'after_title' => '</h4></div>',
+                        )
+                );
+            }
+        }
 
-		if ( cs_get_option( 'footer_after' ) ) {
-			register_sidebar(
-				array(
-					'id'            => 'footer-after',
-					'name'          => 'Footer Widgets After',
-					'description'   => 'Drag widgets for all of pages sidebar',
-					'before_widget' => '<div class="articlemag_widget %2$s">',
-					'after_widget'  => '<div class="clear"></div></div>',
-					'before_title'  => '<div class="widget-title"><h4>',
-					'after_title'   => '</h4></div>',
-				)
-			);
-		}
+        if (cs_get_option('footer_after')) {
+            register_sidebar(
+                    array(
+                        'id' => 'footer-after',
+                        'name' => 'Footer Widgets After',
+                        'description' => 'Drag widgets for all of pages sidebar',
+                        'before_widget' => '<div class="articlemag_widget %2$s">',
+                        'after_widget' => '<div class="clear"></div></div>',
+                        'before_title' => '<div class="widget-title"><h4>',
+                        'after_title' => '</h4></div>',
+                    )
+            );
+        }
 
-		if ( cs_get_option( 'footer_block_after' ) ) {
-			register_sidebar(
-				array(
-					'id'            => 'footer-block-after',
-					'name'          => 'Footer Block After',
-					'description'   => 'Drag widgets for all of pages sidebar',
-					'before_widget' => '<div class="articlemag_widget %2$s">',
-					'after_widget'  => '<div class="clear"></div></div>',
-					'before_title'  => '<div class="widget-title"><h4>',
-					'after_title'   => '</h4></div>',
-				)
-			);
-		}
+        if (cs_get_option('footer_block_after')) {
+            register_sidebar(
+                    array(
+                        'id' => 'footer-block-after',
+                        'name' => 'Footer Block After',
+                        'description' => 'Drag widgets for all of pages sidebar',
+                        'before_widget' => '<div class="articlemag_widget %2$s">',
+                        'after_widget' => '<div class="clear"></div></div>',
+                        'before_title' => '<div class="widget-title"><h4>',
+                        'after_title' => '</h4></div>',
+                    )
+            );
+        }
 
-		$header_style = cs_get_option( 'header_style' );
-		if ( $header_style == 'left' ) {
-			register_sidebar(
-				array(
-					'id'            => 'cs-logo-right',
-					'name'          => 'Logo Right Area',
-					'description'   => 'Drag widgets for logo right content',
-					'before_widget' => '<div class="%2$s">',
-					'after_widget'  => '<div class="clear"></div></div>',
-					'before_title'  => '<div class="widget-title"><h4>',
-					'after_title'   => '</h4></div>',
-				)
-			);
-		}
+        $header_style = cs_get_option('header_style');
+        if ($header_style == 'left') {
+            register_sidebar(
+                    array(
+                        'id' => 'cs-logo-right',
+                        'name' => 'Logo Right Area',
+                        'description' => 'Drag widgets for logo right content',
+                        'before_widget' => '<div class="%2$s">',
+                        'after_widget' => '<div class="clear"></div></div>',
+                        'before_title' => '<div class="widget-title"><h4>',
+                        'after_title' => '</h4></div>',
+                    )
+            );
+        }
 
-		if ( class_exists( 'bbPress' ) ) {
-			register_sidebar(
-				array(
-					'id'            => 'bbpress-forum',
-					'name'          => 'bbPress Forum',
-					'description'   => 'Drag widgets for bbpress forum sidebar',
-					'before_widget' => '<div class="articlemag_widget %2$s">',
-					'after_widget'  => '<div class="clear"></div></div>',
-					'before_title'  => '<div class="widget-title"><h4>',
-					'after_title'   => '</h4></div>',
-				)
-			);
-		}
+        if (class_exists('bbPress')) {
+            register_sidebar(
+                    array(
+                        'id' => 'bbpress-forum',
+                        'name' => 'bbPress Forum',
+                        'description' => 'Drag widgets for bbpress forum sidebar',
+                        'before_widget' => '<div class="articlemag_widget %2$s">',
+                        'after_widget' => '<div class="clear"></div></div>',
+                        'before_title' => '<div class="widget-title"><h4>',
+                        'after_title' => '</h4></div>',
+                    )
+            );
+        }
+    }
 
-	}
 }
+
 new CSFramework_Sidebars_API();
